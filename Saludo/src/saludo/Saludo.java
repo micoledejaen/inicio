c452977f4b8d485a3c3dd53265e67d5cb2576720
Rosa <micoledejaen@gmail.com>
2017-10-15 09:24:20 +0200

    Saludo duplicado y al Mundo

--- Saludo/src/saludo/Saludo.java
+++ Saludo/src/saludo/Saludo.java
@@ -15,7 +15,8 @@ public class Saludo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     System.out.println("Hola Github");   // TODO code application logic here
+     System.out.println("Hola Github"); 
+     System.out.println("Hola Amigo Mundo Feliz");// TODO code application logic here
     }
     
 }
